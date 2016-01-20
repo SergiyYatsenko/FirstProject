50cc6a9fc1c96ca7d647926cbeeb71c7743686b7
SergiyYatsenko <yatswish@gmail.com>
2016-01-20 19:10:52 +0200

    Relocate DAL, Models, Utils from ScheduledParser project to current one

---
+++ NewsGrabber/DAL/NewsContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ScheduledParser.DAL
+{
+    public class NewsContext : DbContext
+    {
+        public NewsContext()  : base ("NewsContext")
+        {
+        }
+
+        public DbSet<NewsItem> NewsItems { get; set; }
+        public DbSet<NewsImage> Images { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
